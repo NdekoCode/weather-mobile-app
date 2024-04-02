c736a702f7dbd109dee40c834cfab974c3aad2fd
Ndekocode <arickbulakali@gmail.com>
2024-04-02 11:39:38 +0200

    feat: add app style

--- App.jsx
+++ App.jsx
@@ -2,11 +2,12 @@ import { ImageBackground, StyleSheet } from "react-native";
 import Home from "./screens/Home/Home";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import bg from "./assets/background.png";
+import { appStyle } from "./app.style";
 export default function App() {
   return (
-    <ImageBackground source={bg} style={styles.app}>
+    <ImageBackground source={bg} style={appStyle.app}>
       <SafeAreaProvider >
-        <SafeAreaView style={styles.app}>
+        <SafeAreaView style={appStyle.app}>
           <Home />
         </SafeAreaView>
       </SafeAreaProvider>
---
+++ app.style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const appStyle = StyleSheet.create({
+  app: {
+    flex: 1,
+  },
+  imageContainer: {
+    flex: 1,
+  },
+});

081e9ca36bbdb2d922b4a68dfa957bfb49e704af
Ndekocode <arickbulakali@gmail.com>
2024-04-06 10:45:57 +0200

    feat: add time refresh

--- components/Clock/Clock.jsx
+++ components/Clock/Clock.jsx
@@ -1,16 +1,24 @@
-import { View } from 'react-native';
-import { clockStyle } from './clock.style';
-import TextWrapper from '../TextWrapper/TextWrapper';
-import { formatTime } from '../../services/date.service';
+import { View } from "react-native";
+import { clockStyle } from "./clock.style";
+import TextWrapper from "../TextWrapper/TextWrapper";
+import { formatTime } from "../../services/date.service";
+import { useEffect, useState } from "react";
 
-const Clock = ({time}) => {
-
-    const timeFormat = formatTime(time)
+const Clock = () => {
+  const [timeFormat, setTimeFormat] = useState(formatTime());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeFormat(formatTime());
+    }, 60000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
   return (
     <View style={clockStyle.timeType}>
       <TextWrapper>{timeFormat}</TextWrapper>
     </View>
-  )
-}
+  );
+};
 
-export default Clock
+export default Clock;
--- services/date.service.js
+++ services/date.service.js
@@ -1,4 +1,4 @@
-export const formatTime = (time)=>{
+export const formatTime = (time=Date.now())=>{
     const date = new Date(time);
     const hours = String(date.getHours()).padStart(2,0);
     const minutes =String( date.getMinutes()).padStart(2,0);

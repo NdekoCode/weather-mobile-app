961d65f53e75e41a69a1419fb58001c5b689e4f8
Ndekocode <arickbulakali@gmail.com>
2024-04-07 17:09:29 +0200

    add default text weather advanced block

--- components/WeatherAdvanced/WeatherAdvanced.jsx
+++ components/WeatherAdvanced/WeatherAdvanced.jsx
@@ -1,24 +1,23 @@
 import { View } from "react-native";
-import { s } from './weatherAdvanced.style';
+import { s } from "./weatherAdvanced.style";
 const WeatherAdvanced = () => {
   return (
     <View style={s.weatherAdvanced}>
       <View>
-        <TextWrapper>WeatherAdvanced</TextWrapper>
-        <TextWrapper>WeatherAdvanced</TextWrapper>
+        <TextWrapper>SunRise Clock</TextWrapper>
+        <TextWrapper>SunRise</TextWrapper>
       </View>
       <View>
-        <TextWrapper>WeatherAdvanced</TextWrapper>
-        <TextWrapper>WeatherAdvanced</TextWrapper>
+        <TextWrapper>SunSet Clock</TextWrapper>
+        <TextWrapper>SunSet</TextWrapper>
       </View>
 
       <View>
-        <TextWrapper>WeatherAdvanced</TextWrapper>
-        <TextWrapper>WeatherAdvanced</TextWrapper>
+        <TextWrapper>Wind SPeed Value</TextWrapper>
+        <TextWrapper>Wind Speed</TextWrapper>
       </View>
     </View>
   );
 };
 
 export default WeatherAdvanced;
-

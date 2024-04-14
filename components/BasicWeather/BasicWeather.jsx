689e4ebf85d5bdc3faa5f9c3cba225eada03fd8d
Ndekocode <arickbulakali@gmail.com>
2024-04-14 10:58:20 +0200

    feat: add button navigation to forecast

--- components/BasicWeather/BasicWeather.jsx
+++ components/BasicWeather/BasicWeather.jsx
@@ -1,21 +1,27 @@
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity, View } from "react-native";
 import TextWrapper from "../TextWrapper/TextWrapper";
 import { s } from "./basicWeather.style";
 import { getWeatherInterpretation } from "../../services/weather.service";
+import { useNavigation } from "@react-navigation/native";
 import Clock from "../Clock/Clock";
-const BasicWeather = ({ basicWeatherData,weatherLocation }) => {
+const BasicWeather = ({ basicWeatherData, weatherLocation }) => {
   const temperature = basicWeatherData.temperature;
   const weatherCode = getWeatherInterpretation(basicWeatherData.weathercode);
+  const router = useNavigation();
+  const onNavigation = () => {
+    router.navigate("Forecast");
+  };
   return (
     <>
-
-      <Clock time={basicWeatherData.time}/>
+      <Clock time={basicWeatherData.time} />
       <TextWrapper style={s.city}>{weatherLocation}</TextWrapper>
       {weatherCode?.label && (
         <TextWrapper style={s.typeText}>{weatherCode?.label}</TextWrapper>
       )}
       <View style={s.temperatureContainer}>
-        <TextWrapper style={s.temperature}>{temperature}</TextWrapper>
+        <TouchableOpacity onPress={onNavigation}>
+          <TextWrapper style={s.temperature}>{temperature}</TextWrapper>
+        </TouchableOpacity>
         {weatherCode?.image && (
           <Image style={s.image} source={weatherCode.image} />
         )}

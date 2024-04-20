b283173ad9409e920d9f2f50d34383f5a6092759
Ndekocode <arickbulakali@gmail.com>
2024-04-20 10:52:27 +0200

    feat: add notifications

--- App.jsx
+++ App.jsx
@@ -1,26 +1,118 @@
-import { StyleSheet } from "react-native";
+import { Platform } from "react-native";
 import alataRegular from "./assets/fonts/Alata-Regular.ttf";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import AppContainer from "./screens/AppContainer";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import Constants from "expo-constants";
+import { useEffect, useRef, useState } from "react";
+import {
+  handleRegistrationError,
+  sendPushNotification,
+} from "./services/notification.service";
 const navigationTheme = {
   colors: {
     background: "transparent",
   },
 };
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const {
+      status: existingStatus,
+    } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      handleRegistrationError(
+        "Permission not granted to get push token for push notification!"
+      );
+      return;
+    }
+    const projectId =
+      Constants?.expoConfig?.extra?.eas?.projectId ??
+      Constants?.easConfig?.projectId;
+    if (!projectId) {
+      handleRegistrationError("Project ID not found");
+    }
+    try {
+      const pushTokenString = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(pushTokenString);
+      return pushTokenString;
+    } catch (e) {
+      handleRegistrationError(`${e}`);
+    }
+  } else {
+    handleRegistrationError("Must use physical device for push notifications");
+  }
+}
 export default function App() {
   const [isFontLoaded] = useFonts({
     "Alata-Regular": alataRegular,
   });
+
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(undefined);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      sendPushNotification(expoPushToken);
+    }, 600_000);
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error) => setExpoPushToken(`${error}`));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      }
+    );
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+
+    return () => {
+      clearInterval(timer);
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
   return (
     <NavigationContainer theme={navigationTheme}>
-      {isFontLoaded && (<AppContainer />)}
+      {isFontLoaded && <AppContainer />}
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  app: {
-    flex: 1,
-  },
-});
---
+++ services/notification.service.js
@@ -0,0 +1,25 @@
+export async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: token,
+    title: "Info Meteo",
+    body: "Hello, we hope the weather's fine",
+    sound: "default",
+    data: {
+      infos: "We help you stay informed about your weather situation !",
+    },
+  };
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+}
+export function handleRegistrationError(errorMessage) {
+  alert(errorMessage);
+  throw new Error(errorMessage);
+}
